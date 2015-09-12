fd98f61feaab2bed05b635b03a88e1dd341a9eb2
Abhi Agarwal <me@abhiagarwal.com>
2015-09-11 23:03:13 -0400

    Pep8

--- axxet/axxet/apps/common/serializers.py
+++ axxet/axxet/apps/common/serializers.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+
 # Serializers define the API representation.
-
-
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
